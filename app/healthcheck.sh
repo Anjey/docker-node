ad7d97119b59bbccbffec015a73fdb6ec4007123
System Administrator <root@MacBook-Pro.local>
2021-09-09 10:31:10 +0300

    order conf

--- app/healthcheck.sh
+++ app/healthcheck.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-curl -sS 127.0.0.1:${PORT}/health || exit 1
+curl -sS 127.0.0.1:${PORT:-8080}/health || exit 1
 
--- app/server.js
+++ app/server.js
@@ -8,16 +8,19 @@ const ipAddress = ip.address();
 const host = '0.0.0.0';
 var hostname = os.hostname();
 
+
+
+
 const app = express();
 app.get('/', (req, res) => {
-  res.send(`Hello World from ip: ${ipAddress} on hostname: ${hostname}`);
+  res.send(`Hello World from ip: ${ipAddress}:${listenPort} on hostname: ${hostname}`);
 });
 app.get("/health", (req, res) => {
   res.send("OK");
 });
 
+
 const listenPort = process.env.PORT || 8080;
 app.listen(listenPort, () => {
   console.log(`running on http://${ipAddress}:${listenPort}`);
 });
-
